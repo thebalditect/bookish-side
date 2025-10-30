05cf8b880aa41a6146e508931d2867b7ae4a886d
Mandar Dharmadhikari <codidharma@gmail.com>
2025-10-30 21:05:42 +0530

    feat: add result class implementation and associated tests

---
+++ src/common/MyBookishSide.Common.Domain/Result.cs
@@ -0,0 +1,52 @@
+namespace MyBookishSide.Common.Domain;
+
+public class Result
+{
+    public bool IsSuccess { get; }
+    public bool IsFailure => !IsSuccess;
+    public List<Error>? Errors { get; private set; }
+
+    protected Result(bool isSuccess, List<Error>? errors)
+    {
+        IsSuccess = isSuccess;
+        Errors = errors ?? null;
+    }
+
+    public static Result Success()
+    {
+        return new Result(true, null);
+    }
+
+    public static Result Failure(List<Error> errors)
+    {
+        return errors.Count == 0
+            ? throw new InvalidOperationException("Failure result should contain at least one error.")
+            : new Result(false, errors);
+    }
+
+    public static Result<T> Success<T>(T value)
+    {
+        return new Result<T>(value, true, null);
+    }
+
+    public static Result<T> Failure<T>(List<Error> errors)
+    {
+        return errors.Count == 0
+            ? throw new InvalidOperationException("Failure result should contain at least one error.")
+            : new Result<T>(default, false, errors);
+    }
+}
+
+public class Result<T> : Result
+{
+    private readonly T? _value;
+
+    public T? Value => IsSuccess
+        ? _value
+        : throw new InvalidOperationException("The property named Value can not be accessed for failure result.");
+
+    public Result(T? value, bool isSuccess, List<Error>? errors) : base(isSuccess, errors)
+    {
+        _value = value;
+    }
+}
---
+++ tests/common/MyBookishSide.Common.Domain.UnitTests/DummyReturnValue.cs
@@ -0,0 +1,3 @@
+namespace MyBookishSide.Common.Domain.UnitTests;
+
+public record DummyReturnValue(string ReturnValue);
